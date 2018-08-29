cc979aff0c901fa08d5cc4cac13666081b650041
Miki Mandoza <mickyastesfamichael@gmail.com>
2018-08-29 16:23:38 +0300

    Elder and database connection classes created

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,20 @@
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import java.sql.SQLException;
+
+class DBConnection {
+    private static ConnectionSource connectionSource;
+    static {
+        // put the path to your local database
+        String dbURL = "jdbc:sqlite:";
+        try{
+            connectionSource = new JdbcConnectionSource(dbURL);
+        } catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static ConnectionSource getConnectionSource() {
+        return connectionSource;
+    }
+}
