fb9ac189da514a5c02a2f5a06c7c6bcf6ba998c3
Miki Mandoza <mickyastesfamichael@gmail.com>
2018-09-13 13:50:30 +0300

    Excel doc that contains sheets, with the neccessary header information, for each congregation prepared

--- src/main/java/com/publictalkgenerator/Constants.java
+++ src/main/java/com/publictalkgenerator/Constants.java
@@ -1,7 +1,24 @@
 package com.publictalkgenerator;
 
-public class Constants {
+import java.util.HashMap;
 
+public class Constants {
+    public static final HashMap<Integer, String> AMMonths;
+    static {
+        AMMonths = new HashMap<>();
+        AMMonths.put(1, "ጥር");
+        AMMonths.put(2, "የካቲት");
+        AMMonths.put(3, "መጋቢት");
+        AMMonths.put(4, "ሚያዝያ");
+        AMMonths.put(5, "ግንቦት");
+        AMMonths.put(6, "ሰኔ");
+        AMMonths.put(7, "ሐምሌ");
+        AMMonths.put(8, "ነሐሴ");
+        AMMonths.put(9, "መስከረም");
+        AMMonths.put(10, "ጥቅምት");
+        AMMonths.put(11, "ህዳር");
+        AMMonths.put(12, "ታህሳሥ");
+    }
     public static final int MINIMUM_FREE_WEEKS = 3;
     public static final int MINIMUM_ELDERS_LEFT_IN_CONG = 2;
     public static final double PERCENTAGE_OF_FREE_CONGREGATIONS_IN_A_WEEK = 0.5;
